2512d10b66c3be8afd45ae0f10fd7ddf2f0bbb45
akmnr <akmn@reso-med.com>
2021-10-05 08:11:33 +0500

    init test vue site

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+    <div>
+      <b-button @click="onClick">Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bitrix24 from "../Bitrix24";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+
+  data() {
+    return { sources: [], bitrix: null };
+  },
+
+  methods: {
+    async onClick() {
+      this.sources = await this.bitrix.getSources();
+    },
+  },
+
+  mounted() {
+    this.bitrix = new Bitrix24();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
